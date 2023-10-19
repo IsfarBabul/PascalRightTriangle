864be9305f347b205bbebed5502010ebdcdee7f5
BT_2N2_15 <bths@>
2023-10-19 13:12:40 -0400

    Scanner

--- src/PascalTriangle.java
+++ src/PascalTriangle.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class PascalTriangle {
+public class PascalTriangle {
+
 }
--- src/PascalTriangleRunner.java
+++ src/PascalTriangleRunner.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class PascalTriangleRunner {
+import java.util.Scanner;
+public class PascalTriangleRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Up to what positive integer power should be printed? ");
+        int number = scan.nextInt();
+        System.out.print("Enable printing powers of previous rows (t/f): ");
+        Boolean printPrevRow = scan.nextBoolean();
+    }
 }
